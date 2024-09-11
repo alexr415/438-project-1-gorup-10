a1dbd14b1f468c159d635133d048992436ba37b4
FranciscoCamach0 <129994954+FranciscoCamach0@users.noreply.github.com>
2024-09-10 22:02:33 -0700

    sign up / log in includes a button for log out to use once logged in can be copied and pasted into other pages once logged in to sign out.

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,52 @@
-import { Text, View } from "react-native";
-
+import { Text, View, Button, StyleSheet, ScrollView, TextInput, Image } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 export default function Index() {
   return (
+
+    const moveH = useNavigation();//once logged in will move you to home
+    const signOut = useNavigation();//for later use once u are logged in
+
+     const logInButton = () => {
+        navigation.moveH('homePage');
+     }
+      const signOutButton = () => {
+             navigation.moveH('index');
+          }
     <View
+
       style={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+
+       <Text>USERNAME</Text>
+       <TextInput
+       style={styles.input}
+       placeholder = "Name"
+       />
+
+       <Text>PASSWORD</Text>
+       <TextInput
+       style={styles.input}
+      placeholder = "Pass"
+      secureTextEntry={true}
+      />
+
+      <Button
+        title="Sign up"
+        onPress={logInButton}
+      />
+
+        <Button
+          title="Sign out"
+          onPress={signOutButton}
+        />
+
+
+
+
     </View>
   );
 }
