01333f57e12b651dd67cc3b91ffd10c316ed5671
AndAgGit <afon.cyffrous@gmail.com>
2024-09-20 00:05:16 -0700

    signUPpage test

---
+++ tests/signUPpage.test.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { render } from '@testing-library/react-native';
+import SignUp from '../app/signUPpage';
+
+jest.mock('expo-sqlite', () => ({
+    openDatabaseAsync: jest.fn(() => ({
+        execSync: jest.fn(),
+        transaction: jest.fn(),
+        getFirstAsync: jest.fn(() => ({
+            
+        })),
+    })),
+}));
+
+jest.mock('@react-navigation/native', () => ({
+    useNavigation: () => ({
+        navigate: jest.fn(),
+    }),
+    useRoute: () => ({
+        params:{
+            user: {
+                id: 100,
+            },
+        }
+    }),
+}));
+
+test('should render the favoritesPage page', () => {
+    const { getByTestId } = render(<SignUp />);
+
+    const username = getByTestId('username');
+    const usernameInput = getByTestId('usernameInput');
+    const password = getByTestId('password');
+    const passwordInput = getByTestId('passwordInput');
+    const confirm = getByTestId('confirm');
+    const confirmInput = getByTestId('confirmInput');
+    const signUp = getByTestId('signUp');
+
+    expect(username).toBeTruthy();
+    expect(usernameInput).toBeTruthy();
+    expect(password).toBeTruthy();
+    expect(passwordInput).toBeTruthy();
+    expect(confirm).toBeTruthy();
+    expect(confirmInput).toBeTruthy();
+    expect(signUp).toBeTruthy();
+});
